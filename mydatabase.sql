0bf3f8141eabda33be7af48968b032388410a05f
Nontlahla <61185332+Nontlahla@users.noreply.github.com>
2020-02-19 14:00:23 +0200

    Add files via upload

---
+++ mydatabase.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM myplace.chippers
+LIMIT 0, 1000
+
+-- Date: 2020-02-19 14:00
+*/
+INSERT INTO `` (`Type`,`Barcode`,`Weight`) VALUES ('CHetos','2290042892','732');
+INSERT INTO `` (`Type`,`Barcode`,`Weight`) VALUES ('Pringles','2290042892','742');
+INSERT INTO `` (`Type`,`Barcode`,`Weight`) VALUES ('Doritos','2840046854','200');
+INSERT INTO `` (`Type`,`Barcode`,`Weight`) VALUES ('Lays','2840011054','200');
+INSERT INTO `` (`Type`,`Barcode`,`Weight`) VALUES ('Simba','6001087313087','0.002');
